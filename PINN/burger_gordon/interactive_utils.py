eb897904945aa8154b316513c59fb2324eb99ca7
erlebach <gordon.erlebach@gmail.com>
2023-02-01 13:29:17 -0500

    Cleaned up Burger code.
    Put all functions in one file (referred to by impl. prefix.)
    DictX.py: singleton to store a global dicitonary (GlobDct())

---
+++ PINN/burger_gordon/interactive_utils.py
@@ -0,0 +1,10 @@
+"""
+Wrappers with widgets to improve the presentation. 
+Author: Gordon Erlebacher
+Date: 2023-02-01
+"""
+
+import ipywidgets as widgets
+import Fall_2022_Burgers_Equation_functions as impl
+
+
